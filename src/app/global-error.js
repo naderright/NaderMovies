21c4cdff8bb2d773d4e457624a0ad7e3ae019696
Nader Mohamed <Nadercs2018@gmail.com>
2025-01-03 23:16:04 +0200

    add error

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -4,9 +4,9 @@ export default function GlobalError({ error, reset }) {
   return (
     // global-error must include html and body tags
     <html>
-      <body>
+      <body className=" flex justify-center items-center ">
         <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <button  className='underline text-blue-500' onClick={() => reset()}>Try again</button>
       </body>
     </html>
   )
